160e018f1f9489f175788329de67bdf3e1e56ab3
n9d <39367157+n9d@users.noreply.github.com>
2019-11-17 15:19:49 +0900

    vscodeで全部整形

---
+++ vscode-extension-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+code --install-extension aeschli.vscode-css-formatter
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension joelday.docthis
+code --install-extension MS-CEINTL.vscode-language-pack-ja
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension oderwat.indent-rainbow
+code --install-extension SimonSiefke.svg-preview
+code --install-extension sohamkamani.code-eol
