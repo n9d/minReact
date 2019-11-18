8b8f6b6a86f78026836ad9b9b251c30917144163
n9d <39367157+n9d@users.noreply.github.com>
2019-11-18 14:32:53 +0900

    index.html作り直し

---
+++ mkindex.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cat << HEAD > index.html
+<html>
+  <head>
+    <title>最小構成でReactJS</title>
+  </head>
+  <body>
+    <p> 詳細は<a href="https://github.com/n9d/minReact">minReact</a>を参照のこと </p>
+    <p />
+HEAD
+
+for file in *.html; do
+  if [ $file != index.html ]; then
+    title=`cat $file | ruby -e 'print STDIN.read.match(/<title>([^<]*)<\/title>/m)[1]'`
+    cat $file | ruby -e 'print STDIN.read.match(/<title>([^<]*)<\/title>/m)[1]'
+    echo "    <p> <a href='${file}'>${title}</a> ${file} </p>" >> index.html
+  fi
+done
+
+cat << TAIL >> index.html
+  </body>
+</html>
+TAIL
